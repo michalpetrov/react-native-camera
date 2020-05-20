fd1ca1b4f3e14fd30ff1703c8725f3021004633a
Michal Petrov <michal.petrov@gmail.com>
2020-05-20 16:05:56 +0200

    android save image

--- android/src/general/java/org/reactnative/camera/tasks/FrameSaverAsyncTask.java
+++ android/src/general/java/org/reactnative/camera/tasks/FrameSaverAsyncTask.java
@@ -1,8 +1,14 @@
 package org.reactnative.camera.tasks;
 
+import android.graphics.BitmapFactory;
+import android.graphics.ImageFormat;
+import android.graphics.Rect;
+import android.graphics.YuvImage;
+import android.hardware.Camera;
 import android.net.Uri;
 import android.util.Log;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.util.UUID;
@@ -26,10 +32,24 @@ public class FrameSaverAsyncTask extends android.os.AsyncTask<Void, Void, String
         }
         File imageFile;
         try {
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inJustDecodeBounds = true;
+            BitmapFactory.decodeByteArray(mImageData, 0, mImageData.length, options);
+
             imageFile = File.createTempFile(UUID.randomUUID().toString(), ".jpg", mCacheDir);
             imageFile.createNewFile();
+
+            YuvImage image = new YuvImage(mImageData, ImageFormat.NV21, options.outWidth, options.outHeight, null);
+            Rect rectangle = new Rect();
+            rectangle.bottom = options.outHeight;
+            rectangle.top = 0;
+            rectangle.left = 0;
+            rectangle.right = options.outWidth;
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            image.compressToJpeg(rectangle, 100, baos);
+
             FileOutputStream fOut = new FileOutputStream(imageFile);
-            fOut.write(mImageData);
+            fOut.write(baos.toByteArray());
             fOut.flush();
             fOut.close();
             return Uri.fromFile(imageFile).toString();
