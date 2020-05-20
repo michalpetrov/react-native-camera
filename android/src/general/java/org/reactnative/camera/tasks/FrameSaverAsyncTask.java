497e7f2a686b4515022c15e46a95670bb25f9969
Michal Petrov <michal.petrov@gmail.com>
2020-05-20 16:30:52 +0200

    android save image

--- android/src/general/java/org/reactnative/camera/tasks/FrameSaverAsyncTask.java
+++ android/src/general/java/org/reactnative/camera/tasks/FrameSaverAsyncTask.java
@@ -8,18 +8,27 @@ import android.hardware.Camera;
 import android.net.Uri;
 import android.util.Log;
 
+import com.google.android.gms.vision.Frame;
+
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.nio.ByteBuffer;
 import java.util.UUID;
 
 public class FrameSaverAsyncTask extends android.os.AsyncTask<Void, Void, String> {
     private byte[] mImageData;
+    private int mWidth;
+    private int mHeight;
+    private int mRotation;
     private File mCacheDir;
     private FrameSaverAsyncTaskDelegate mDelegate;
 
-    public FrameSaverAsyncTask(FrameSaverAsyncTaskDelegate delegate, byte[] imageData, File cacheDir) {
+    public FrameSaverAsyncTask(FrameSaverAsyncTaskDelegate delegate, byte[] imageData, int width, int height, int rotation, File cacheDir) {
         mImageData = imageData;
+        mWidth = width;
+        mHeight = height;
+        mRotation = rotation;
         mCacheDir = cacheDir;
         mDelegate = delegate;
     }
@@ -32,19 +41,38 @@ public class FrameSaverAsyncTask extends android.os.AsyncTask<Void, Void, String
         }
         File imageFile;
         try {
-            BitmapFactory.Options options = new BitmapFactory.Options();
-            options.inJustDecodeBounds = true;
-            BitmapFactory.decodeByteArray(mImageData, 0, mImageData.length, options);
+//            Frame.Builder builder = new Frame.Builder();
+//            ByteBuffer byteBuffer = ByteBuffer.wrap(mImageData);
+//            builder.setImageData(byteBuffer, mWidth, mHeight, ImageFormat.NV21);
+//            switch (mRotation) {
+//                case 90:
+//                    builder.setRotation(Frame.ROTATION_90);
+//                    break;
+//                case 180:
+//                    builder.setRotation(Frame.ROTATION_180);
+//                    break;
+//                case 270:
+//                    builder.setRotation(Frame.ROTATION_270);
+//                    break;
+//                default:
+//                    builder.setRotation(Frame.ROTATION_0);
+//            }
+//            Frame frame = builder.build()
+
+
+//            BitmapFactory.Options options = new BitmapFactory.Options();
+//            options.inJustDecodeBounds = true;
+//            BitmapFactory.decodeByteArray(mImageData, 0, mImageData.length, options);
 
             imageFile = File.createTempFile(UUID.randomUUID().toString(), ".jpg", mCacheDir);
             imageFile.createNewFile();
 
-            YuvImage image = new YuvImage(mImageData, ImageFormat.NV21, options.outWidth, options.outHeight, null);
+            YuvImage image = new YuvImage(mImageData, ImageFormat.NV21, mWidth, mHeight, null);
             Rect rectangle = new Rect();
-            rectangle.bottom = options.outHeight;
+            rectangle.bottom = mHeight;
             rectangle.top = 0;
             rectangle.left = 0;
-            rectangle.right = options.outWidth;
+            rectangle.right = mWidth;
             ByteArrayOutputStream baos = new ByteArrayOutputStream();
             image.compressToJpeg(rectangle, 100, baos);
 
